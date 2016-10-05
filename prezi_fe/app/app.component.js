d69eacdeaaebc8f4bc3c96d1213c0944d04f539e
juanca <jcrodriguezu@gmail.com>
2016-10-05 00:21:39 -0500

    Continue adding the frontend logic

--- prezi_fe/app/app.component.js
+++ prezi_fe/app/app.component.js
@@ -9,13 +9,59 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var Slide = (function () {
+    function Slide() {
+    }
+    return Slide;
+}());
+exports.Slide = Slide;
+var SLIDES = [
+    {
+        "id": "56f137f432fbb67217182785",
+        "title": "incididunt amet ad nostrud",
+        "thumbnail": "https://placeimg.com/400/400/any",
+        "createdAt": "March 6, 2014"
+    },
+    {
+        "id": "56f137f4d62116d1231786ca",
+        "title": "Lorem commodo excepteur minim",
+        "thumbnail": "https://placeimg.com/400/400/any",
+        "createdAt": "July 31, 2015"
+    },
+    {
+        "id": "56f137f46ba885ffacf4d3ff",
+        "title": "ut ipsum ut nostrud",
+        "thumbnail": "https://placeimg.com/400/400/any",
+        "createdAt": "July 5, 2015"
+    },
+    {
+        "id": "56f137f48510226968e8c9e7",
+        "title": "anim id enim duis",
+        "thumbnail": "https://placeimg.com/400/400/any",
+        "createdAt": "July 11, 2015"
+    },
+    {
+        "id": "56f137f40684a0672110741c",
+        "title": "consectetur dolor nisi amet",
+        "thumbnail": "https://placeimg.com/400/400/any",
+        "createdAt": "May 3, 2015"
+    }
+];
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'Prezi Slides';
+        this.slides = SLIDES;
+        this.slide = {
+            id: '1',
+            title: 'Test title',
+            thumbnail: 'test thumb',
+            createdAt: 'date'
+        };
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>Prezi Slides</h1>'
+            template: "\n    <h1>{{title}}</h1>\n    <h2>{{slide.title}} details!</h2>\n    <div><label>id: </label>{{slide.id}}</div>\n    <div><label>title: </label>{{slide.title}}</div>\n    <div><label>thumbnail: </label>{{slide.thumbnail}}</div>\n    <div><label>created at: </label>{{slide.createdAt}}</div>\n    <h2>The Slides</h2>\n    <ul class=\"slides\">\n        <li *ngFor=\"let slide of slides\">\n            <span class=\"badge\">{{slide.id}}</span> {{slide.title}}\n        </li>\n    </ul>\n    "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- prezi_fe/app/app.component.ts
+++ prezi_fe/app/app.component.ts
@@ -1,8 +1,70 @@
 import { Component } from '@angular/core';
 
+export class Slide {
+    id: string;
+    title: string;
+    thumbnail: string;
+    createdAt: string;
+}
+
+const SLIDES: Slide[] = [
+  {
+    "id": "56f137f432fbb67217182785",
+    "title": "incididunt amet ad nostrud",
+    "thumbnail": "https://placeimg.com/400/400/any",
+    "createdAt": "March 6, 2014"
+  },
+  {
+    "id": "56f137f4d62116d1231786ca",
+    "title": "Lorem commodo excepteur minim",
+    "thumbnail": "https://placeimg.com/400/400/any",
+    "createdAt": "July 31, 2015"
+  },
+  {
+    "id": "56f137f46ba885ffacf4d3ff",
+    "title": "ut ipsum ut nostrud",
+    "thumbnail": "https://placeimg.com/400/400/any",
+    "createdAt": "July 5, 2015"
+  },
+  {
+    "id": "56f137f48510226968e8c9e7",
+    "title": "anim id enim duis",
+    "thumbnail": "https://placeimg.com/400/400/any",
+    "createdAt": "July 11, 2015"
+  },
+  {
+    "id": "56f137f40684a0672110741c",
+    "title": "consectetur dolor nisi amet",
+    "thumbnail": "https://placeimg.com/400/400/any",
+    "createdAt": "May 3, 2015"
+  }
+];
+
 @Component({
   selector: 'my-app',
-  template: '<h1>Prezi Slides</h1>'
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{slide.title}} details!</h2>
+    <div><label>id: </label>{{slide.id}}</div>
+    <div><label>title: </label>{{slide.title}}</div>
+    <div><label>thumbnail: </label>{{slide.thumbnail}}</div>
+    <div><label>created at: </label>{{slide.createdAt}}</div>
+    <h2>The Slides</h2>
+    <ul class="slides">
+        <li *ngFor="let slide of slides">
+            <span class="badge">{{slide.id}}</span> {{slide.title}}
+        </li>
+    </ul>
+    `
 })
 
-export class AppComponent { }
+export class AppComponent {
+    title = 'Prezi Slides';
+    slides = SLIDES;
+    slide: Slide = {
+        id: '1',
+        title: 'Test title',
+        thumbnail: 'test thumb',
+        createdAt: 'date'
+    };
+}
